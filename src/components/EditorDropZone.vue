3a7964b4a65d56559c6c723c337d3abf601e33b8
holubv <holubvojtech@gmail.com>
2021-03-16 07:49:36 +0100

    Fix dropzone

--- src/components/EditorDropZone.vue
+++ src/components/EditorDropZone.vue
@@ -118,32 +118,32 @@
                     this.showDropInput = false;
                 }
             },
-            dropZoneTemplate: function () {
-                return '';
-            },
             // dropZoneTemplate: function () {
-            //     return `<div class="dz-preview dz-file-preview">
-            //     <div class="dz-image">
-            //         <div data-dz-thumbnail-bg></div>
-            //     </div>
-            //
-            //     <div class="dz-details">
-            //         <div class="name-wrapper">
-            //             <div class="dz-filename"><span data-dz-name></span></div>
-            //         </div>
-            //         <div class="status-wrapper">
-            //             <div class="dz-size"><span data-dz-size></span></div>
-            //             <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>
-            //             <div class="icon-wrapper">
-            //                 <div class="dz-success-mark"><i class="fas fa-check"></i></div>
-            //                 <div class="dz-error-mark"><i class="fas fa-times"></i></div>
-            //             </div>
-            //         </div>
-            //     </div>
-            //
-            //     <div class="dz-error-message"><span data-dz-errormessage></span></div>
-            // </div>`
+            //     return '';
             // },
+            dropZoneTemplate: function () {
+                return `<div class="dz-preview dz-file-preview">
+                <div class="dz-image">
+                    <div data-dz-thumbnail-bg></div>
+                </div>
+
+                <div class="dz-details">
+                    <div class="name-wrapper">
+                        <div class="dz-filename"><span data-dz-name></span></div>
+                    </div>
+                    <div class="status-wrapper">
+                        <div class="dz-size"><span data-dz-size></span></div>
+                        <div class="dz-progress"><span class="dz-upload" data-dz-uploadprogress></span></div>
+                        <div class="icon-wrapper">
+                            <div class="dz-success-mark"><i class="fas fa-check"></i></div>
+                            <div class="dz-error-mark"><i class="fas fa-times"></i></div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="dz-error-message"><span data-dz-errormessage></span></div>
+            </div>`
+            },
         }
     }
 </script>
